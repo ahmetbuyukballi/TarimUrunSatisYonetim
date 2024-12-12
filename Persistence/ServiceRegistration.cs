074d6bc770d49b34f717aefe828b8914e20fbd81
yusufkolus <ykkolus46@gmail.com>
2024-12-12 21:37:42 +0300

    yeni entity

---
+++ Persistence/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistence(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer("Server=192.168.1.104\\MSSQLSERVER_2022;Database=TarimDb;User Id=sa;Password=123456;TrustServerCertificate=True;"));
+        }
+       
+    }
+}
